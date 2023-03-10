7ae9446492276e14cfb98b95ae7f984fc0eaa346
Augustin Bost <augustin.bost@epitech.eu>
2023-03-10 22:34:58 +0100

    FILE augustin with menu

---
+++ augustin/conf.lua
@@ -0,0 +1,14 @@
+function love.conf( t )
+    t.window.width = 1920
+    t.window.height = 1080
+
+    t.window.minwidth = 1920
+    t.window.minheight = 1080
+
+    t.title = "Dungeon Taker"
+
+    t.window.resizable = false
+    t.window.borderless = false
+
+    t.identity = "GAME JAM TEK GANG COUTEAUX"
+end
