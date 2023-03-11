ac4d11b3eb6c1755efcf4a4f03531428d1586220
alexis <alexis.boitel@epitech.eu>
2023-03-11 14:50:55 +0100

    push

---
+++ sprites/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tileset" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="tileset.png" width="64" height="64"/>
+</tileset>
