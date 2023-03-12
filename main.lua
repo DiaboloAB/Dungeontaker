d2e4e5e4b647e00ff516367c037039154bc688c2
Yasmine BEDRANE <yasmine.bedrane@epitech.eu>
2023-03-12 01:23:49 +0100

    [ADD] function for slime and audio

--- main.lua
+++ main.lua
@@ -1,22 +1,25 @@
 local love = require 'love'
 require "src/menu"
+require "src/audio"
 require "src/player"
 require "src/map"
 require "src/entities"
 require "src/entity"
 require "src/game_over"
 require "src/ghost"
+require "src/slime"
 require "src/pause"
 
 function love.load()
-    over = 1
     love.graphics.setDefaultFilter( "nearest" )
     in_start_menu = true;
     in_game_over = true;
+    audio_load()
     menu_load()
     map_load()
     player_load()
     ghost_load()
+    slime_load()
     pause_load()
     game_over_load()
 end
@@ -28,6 +31,7 @@ function love.update()
     else
         player_update()
         ghost_update()
+        slime_update()
     end
 end
 
@@ -38,6 +42,7 @@ function love.draw()
         map_draw()
         player_draw()
         ghost_draw()
+        slime_draw()
     end
     if life < 0 then
         game_over_draw()
@@ -45,7 +50,7 @@ function love.draw()
             reset()
         end
     end
-    if (pause_button == false and over == 1) then
+    if (pause_button == false and life > 0) then
         pause_draw()
     end
 end
@@ -54,6 +59,14 @@ function love.mousepressed(x, y, button, istouch, key)
     if in_start_menu == true then
         button_menu(x, y, button, key)
     end
+
+    if start_audio == true then
+        love.audio.play(source)
+    end
+    if start_audio == false then
+        love.audio.pause(source)
+    end
+
     if pause_button == false then
         pause_menu(x, y, button, key)
     end
@@ -64,6 +77,7 @@ function love.keypressed( key )
     if in_start_menu == false then
         move_player( key )
         move_ghost( key )
+        move_slime( key )
     if pause_button == true then
             pause_menu_true( key )
             goto ici
